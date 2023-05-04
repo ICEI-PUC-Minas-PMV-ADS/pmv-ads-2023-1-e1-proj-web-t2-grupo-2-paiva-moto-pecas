7463914734816be86443412b6f037160562a60a4
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-04 00:04:04 -0300

    ✬ Add banco de dados de produtos

---
+++ src/produtos/database/prdutos-db.js
@@ -0,0 +1,343 @@
+export const produtos = [
+    {
+        nome: 'Rabeta traseira (para-lama)',
+        apresentacao: `
+        Rabeta traseira para-lama para moto fan start 160 e CG 150        
+        `,
+        descricao: `
+        Rabeta traseira para-lama fan start 160, CG 150.        
+        Aplicação:
+        Fan 125 - 2014 em diante, Fan 150 - 2014 em diante,  Fan 160 - 2014 em diante
+        Start 150 - 2014/2015, Start 160 - 2016 em diante, Titan 150 - 2014/2015, Titan 160 - 2016 em diante
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '1',
+        setor: "Produto"
+    },
+    {
+        nome: 'Caixa Direção CG Honda',
+        apresentacao: 'Caixa de direção original honda CG 125, CG 150, CG 160',
+        descricao: `
+        Caixa de direção original honda; 
+        Material: Alumínio; 
+        Posição: Superior/Inferior; 
+        Diâmetro do tubo de direção: 26 mm; 
+        Diâmetro do garfo: 1 mm; 
+        Diâmetro do guidão: 1 mm. 
+        Aplicação: Cg fan 125 - cg titan 125 - cg fan 150 - cg titan 150 - cg fan 160 - cg titan 160 - cg start 160
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '2',
+        setor: "Produto"
+    },
+    {
+        nome: 'Borracha Pedaleira Estribo Original',
+        apresentacao: `
+        Par borracha estribo para CG Titan, Fan 125 e 150.          
+        `,
+        descricao: `
+        Par borracha estribo cg / today / titan 125 / 150 / fan / mix
+        Material: borracha; 
+        posição: traseiro/dianteiro; 
+        comprimento x largura: 10 cm x 10 cm
+        Aplicação: Titan 125  - titan 150 - fan 125 - cg 125 
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '3',
+        setor: "Produto"
+    },
+    {
+        nome: 'Amortecedor Traseiro Bros 160',
+        apresentacao: `
+        Amortecedor traseiro Bros 150 original Honda.          
+        `,
+        descricao: `
+        Amortecedor traseiro Bros 150 original Honda, 
+        Posição: Traseiro; 
+        Diâmetro: 20 cm; 
+        Comprimento: 30 cm;
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '4',
+        setor: "Produto"
+    },
+    {
+        nome: 'Cabo Acelerador B Shadow 750',
+        apresentacao: `
+        Cabo Acelerador B Shadow 750 2011 2012 2013 2014 Honda.         
+        `,
+        descricao: `       
+        Tipo de veículo: Moto/Quadriciclo; 
+        Material do cabo de acelerador: 1; 
+        Comprimento da capa do cabo de acelerador: 1 cm
+        Aplicação: Shadow 750: 2011,2012, 2013, 2014
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '5',
+        setor: "Produto"
+    },
+    {
+        nome: 'Disco De Freio Traseiro Honda Cb 500',
+        apresentacao: `
+        Disco Freio Traseiro Honda Cb 500 Ano 1997 Até 2005.         
+        `,
+        descricao: `       
+        Disco Freio Traseiro Honda Cb 500 Ano 1997 Até 2005, 
+        Posição: Traseiro; 
+        Tipo de disco de freio: Ventilado; 
+        Diâmetro externo: 1 mm.
+        `,
+        marca: 'Honda',
+        categoria: 'Chassis',
+        imagem: '6',
+        setor: "Produto"
+    },
+    {
+        nome: 'Embreagem Completa (17 Peças) Yzf R1',
+        apresentacao: ` Embreagem dos discos para moto Yzf R1 Ano 2004 A 2008.`,
+        descricao: `       
+        Embreagem Completa (17 Peças) Yzf R1 Ano 2004 A 2008
+        Tipo de embreagem: Dos discos; 
+        Inclui disco: Sim
+        `,
+        marca: 'Yamaha',
+        categoria: 'Transmissão',
+        imagem: '7',
+        setor: "Produto"
+    },
+    {
+        nome: 'Rolete Neo 115 6 Gramas',
+        apresentacao: `Rolete Neo 115 6 Gramas`,
+        descricao: `       
+        Rolete Neo 115 6 Gramas. 
+        `,
+        marca: 'Yamaha',
+        categoria: 'Transmissão',
+        imagem: '8',
+        setor: "Produto"
+    },
+    {
+        nome: 'Eixo Pinhão Yamaha Xt 660 Ténéré 660 ',
+        apresentacao: `Eixo Pinhão Yamaha Xt 660 Ténéré 660 Original.(Semi Novo)`,
+        descricao: `    
+        (Semi Novo)   
+        Eixo Pinhão Xt 660 Ténéré 660 Original.
+        `,
+        marca: 'Yamaha',
+        categoria: 'Transmissão',
+        imagem: '9',
+        setor: "Produto"
+    },
+    {
+        nome: 'Tampa Do Cvt Nmax',
+        apresentacao: `Tampa Do Cvt Nmax - 2021 Em Diante`,
+        descricao: `    
+        TAMPA DO CVT NMAX - 2021 EM DIANTE -B6HE543100
+        Aplicação: YAMAHA NMAX
+        `,
+        marca: 'Yamaha',
+        categoria: 'Transmissão',
+        imagem: '10',
+        setor: "Produto"
+    },
+    {
+        nome: 'Retentor Do Eixo Embreagem',
+        apresentacao: `Neo 115 2005 Á 2008 Original`,
+        descricao: `    
+        Retentor Do Eixo Embreagem
+        Aplicação: Neo 115 2005 Á 2008 Original Yamaha
+        `,
+        marca: 'Yamaha',
+        categoria: 'Transmissão',
+        imagem: '11',
+        setor: "Produto"
+    },
+    {
+        nome: 'Kit Peças Motor',
+        apresentacao: `Fazer 250 Lander 250 Tenere Xtz250`,
+        descricao: `    
+        Diâmetro do cilindro: 74 mm; 
+        Inclui pistões, aros.
+
+        Conteúdo da embalagem:
+        1 - Kit Cilindro, Pistão, Anéis, Pino e Trava; 
+        1 - Biela Completa Txk; 
+        1 - Jogo de Juntas Vedamotors; 
+        2 - Retentor de Válvulas ; 
+        2 - Rolamento Virabrequim Nachi; 
+        1 - Válvula Admissão Metal Leve/WW3; 
+        1 - Válvula Escape Metal Leve/WW3; 
+        1 - Arvore Comando; 
+        2 – Balancim; 
+        1 - Guia Corrente Comando; 
+        1 - Corrente Comando; 
+        1 - Acionador Corrente Comando.
+        `,
+        marca: 'Metal Leve',
+        categoria: 'Motor',
+        imagem: '12',
+        setor: "Produto"
+    },
+    {
+        nome: 'Kit Cilindro Pistão Anéis',
+        apresentacao: `Titan, nxr 125 2002/2008`,
+        descricao: `    
+        Kit cilindro pistão e anéis metal leve
+        Conteúdo da embalagem: Cilindro, camisa, pistão, anéis de segmento, 
+        pino do pistão, travas do pino do pistão.
+        `,
+        marca: 'Metal Leve',
+        categoria: 'Motor',
+        imagem: '13',
+        setor: "Produto"
+    },
+    {
+        nome: 'Placa De Partida Yamaha',
+        apresentacao: `Ybr 125 E/ Ed 2002 A 2005`,
+        descricao: `    
+        Placa De Partida Yamaha Ybr 125 E/ Ed 2002 A 2005        
+        Aplicação: 
+        Yamaha XTZ 125 E 2002 A 2005; 
+        Yamaha YBR 125 E 2002 A 2005; 
+        Yamaha YBR 125 ED 2002 A 2005
+        `,
+        marca: 'Magnetron',
+        categoria: 'Peças de Partida',
+        imagem: '14',
+        setor: "Produto"
+    },
+    {
+        nome: 'Motor Partida Arranque',
+        apresentacao: `Cg 150, Titan 150, Fan Nxr150`,
+        descricao: `    
+        Motor Partida Honda Titan 150 Magnetron
+        Aplicação:
+        Honda Titan 150;
+        Honda Fan 150;
+        Honda Fan 125;
+        Honda Bros 150;
+        Honda Bros 125.
+        `,
+        marca: 'Magnetron',
+        categoria: 'Peças de Partida',
+        imagem: '15',
+        setor: "Produto"
+    },
+    {
+        nome: 'Placa Partida',
+        apresentacao: `Titan125 Es 99 A 02 Xr 200r 94 A 02`,
+        descricao: `    
+        Placa Partida Magnetron
+        Aplicação:
+        Honda: CBX 200 Strada (1994-2002); 
+        CBX 250 Twister (2001-2008); 
+        CG 125 ES Titan (1999-2002); 
+        XR 200 R (1994-2002); 
+        XR 250 Tornado (2001-2008); 
+        CRF 250 F (2003-2019).
+        `,
+        marca: 'Magnetron',
+        categoria: 'Peças de Partida',
+        imagem: '16',
+        setor: "Produto"
+    },
+    {
+        nome: 'Engrenagem Embreagem Partida',
+        apresentacao: `Bros Cargo Cg Start Titan 160`,
+        descricao: `    
+        Engrenagem Embreagem Partida Bros Cargo Cg Start Titan 160
+        Indicação de Uso: Nxr 160 Bros Esd 2015 - Nxr 160 Bros Edd 2015 até 2021 - 
+        Nxr 160 Bros Es 2017 até 2017, Cg 160 FAN 2016 até 2021 - 
+        Cg 160 Titan 2016 até 2021 - Cg 160 Cargo 2018 a 2021 - 
+        Cg 160 Start 2018 a 2021.
+        `,
+        marca: 'Magnetron',
+        categoria: 'Peças de Partida',
+        imagem: '17',
+        setor: "Produto"
+    },
+    {
+        nome: 'Escapamento street e off road',
+        apresentacao: `Produzido em aço carbono, entrega excelentes níveis de potência.`,
+        descricao: `    
+        Produzido em aço carbono, entrega excelentes níveis de potência 
+        para motocicletas street e off road, com a melhor relação 
+        custo/benefício do mercado. O projeto contempla tubos bem dimensionados, 
+        garantindo maior potência em baixas, médias e altas rotações, 
+        além de retomadas certas nas saídas de curvas
+        `,
+        marca: 'Pro Tork',
+        categoria: 'Escapamento',
+        imagem: '18',
+        setor: "Produto"
+    },
+    {
+        nome: 'Escapamento Moto Cg',
+        apresentacao: `O Escapamento Original Titan 125 Pro Tork.`,
+        descricao: `    
+        O Escapamento Original Titan 125 Pro Tork é fabricado em aço de alta qualidade e resistência para garantir o máximo de segurança , possui uma instalação simples e rápida.
+        Aplicação:
+        Titan 125 KS/KSE; 
+        Fan 125. 
+        `,
+        marca: 'Pro Tork',
+        categoria: 'Escapamento',
+        imagem: '19',
+        setor: "Produto"
+    },
+    {
+        nome: 'Pneu Moto Cb500',
+        apresentacao: `Pneu traseiro 160/60-17 Michelin Pilot Power 2ct`,
+        descricao: `    
+        A largura da seção é de 160mm, possui um diâmetro de 17", altura e 
+        largura é de 60, fabricado em Original Michelin, projetado para alcançar a 
+        firmeza que você precisa ao pilotar, não inclui aro.
+        `,
+        marca: 'Michelin',
+        categoria: 'Pneu',
+        imagem: '20',
+        setor: "Produto"
+    },
+    {
+        nome: 'Pneu dianteira para moto',
+        apresentacao: `Maggion Viper C/c 90/90-19 T 52`,
+        descricao: `    
+        A largura da seção é de 90mm, possui um diâmetro de 19", em relação entre 
+        altura e largura é de 90, recomendado para usar on road/off road, mantém 
+        a estabilidade em pisos molhados, projetado para alcançar a firmeza que 
+        você precisa ao pilotar.
+        `,
+        marca: 'Michelin',
+        categoria: 'Pneu',
+        imagem: '21',
+        setor: "Produto"
+    },
+    {
+        nome: 'Câmaras De Ar Michelin',
+        apresentacao: `Aro 21 + Aro 17 Xt600 - xt660 - gs650`,
+        descricao: `    
+        Câmaras de ar aro 21 - MD 21 e aro 17 - MH 17 - Michelin.
+        São as duas câmaras de ar para as rodas dianteira e traseira.
+        Aplicação:
+        Aprilia;
+        Cagiva;
+        Honda;
+        Husqvarna;        
+        Kawasaki: klr 650;
+        Tengai;
+        Kymco;
+        Moto guzzi;
+        Suzuki;
+        Yamaha.
+        `,
+        marca: 'Michelin',
+        categoria: 'Pneu',
+        imagem: '22',
+        setor: "Produto"
+    },
+]
--- src/produtos/script.js
+++ src/produtos/script.js
@@ -1,1 +1,24 @@
-console.log('teste')
+// import { produtos } from './database/prdutos-db.js'
+
+// const divProduto = document.querySelector('#divProduto');
+
+// function produto(imagem, nome, apresentacao) {
+//     divProduto.innerHTML += `
+//     <div  class="produtoUnit" >    
+//         <img id="img" src="./${imagem}.jpg" alt="">
+//         <h3>${nome}</h3>
+//         <p>${apresentacao}</p>
+//     </div>     
+//     `
+// }
+
+// for (let i = 0; i < produtos.length; i++) {
+//     produto(
+//         produtos[i].imagem,
+//         produtos[i].nome,
+//         produtos[i].apresentacao
+//     )
+// }
+
+
+
