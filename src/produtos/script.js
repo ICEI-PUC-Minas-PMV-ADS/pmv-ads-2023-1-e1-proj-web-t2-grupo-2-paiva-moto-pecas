341920c67a719e36305cb6956e6a6b2532a3e627
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-04 03:59:59 -0300

    ✬ Concluindo lógica dos filtros

--- src/produtos/script.js
+++ src/produtos/script.js
@@ -1,6 +1,10 @@
 import { produtos } from '../database/produtos/produtos-db.js';
 
+const loja = document.querySelector('#loja');
 const divProduto = document.querySelector('#divProduto');
+const honda = document.querySelector('#honda')
+const yamaha = document.querySelector('#yamaha')
+
 
 function printProdutos(imagem, nome, apresentacao) {
     divProduto.innerHTML += `
@@ -12,35 +16,27 @@ function printProdutos(imagem, nome, apresentacao) {
     `
 }
 
-const honda = document.querySelector('#honda')
+function printFilterProdutos(imagem, nome, apresentacao) {
+    loja.innerHTML += `
+    <section class="clasProcutos" id="filterSection">        
+    </section >`
+    const filterSection = document.querySelector('#filterSection')
+    filterSection.innerHTML += `        
+        <div class="produtoUnit" >
+            <img id="img" src="../database/produtos/public/${imagem}.png" alt="">
+            <h3>${nome}</h3>
+            <p>${apresentacao}</p>
+        </div>     
+    `
+}
 
 
 let filtrados
+// let filtradosHonda
+// let filtradosYamaha
 
 
-
-
-
-function change() {
-
-    honda.addEventListener('change', () => {
-        if (honda.checked === true) {
-            filtrados = produtos.filter((produto) => {
-                return produto.marca == 'Honda'
-            })
-        }
-        if (honda.checked === false) {
-            location.reload();
-        }
-        divProduto.innerHTML = ''
-        return filtrados.map((produt) => {
-            printProdutos(
-                produt.imagem,
-                produt.nome,
-                produt.apresentacao
-            )
-        })
-    })
+function initial() {
     return produtos.map((produto) => {
         printProdutos(
             produto.imagem,
@@ -50,9 +46,53 @@ function change() {
     })
 }
 
+function changeHonda() {
+
+    honda.addEventListener('change', () => {
+        if (honda.checked === true) {
+            filtrados = produtos.filter((produto) => {
+                return produto.marca === 'Honda'
+            })
+        }
+        if (honda.checked === false) {
+            location.reload();
+        }
+        divProduto.innerHTML = ''
+        return filtrados.map((produto) => {
+            printFilterProdutos(
+                produto.imagem,
+                produto.nome,
+                produto.apresentacao
+            )
+        })
+    })
+
+}
+
+function changeYamaha() {
+
+    yamaha.addEventListener('change', () => {
+        if (yamaha.checked === true) {
+            filtrados = produtos.filter((produto) => {
+                return produto.marca === 'Yamaha'
+            })
+        }
+        if (yamaha.checked === false) {
+            location.reload();
+        }
+        divProduto.innerHTML = ''
+        return filtrados.map((produto) => {
+            printFilterProdutos(
+                produto.imagem,
+                produto.nome,
+                produto.apresentacao
+            )
+        })
+    })
+
+}
 
 
-
-
-change()
-
+initial()
+changeHonda()
+changeYamaha()
