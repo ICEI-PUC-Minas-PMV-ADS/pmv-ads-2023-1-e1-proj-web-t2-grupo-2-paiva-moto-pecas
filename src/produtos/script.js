73d2c09651e4f1504eaaebe8ad5047bce6427f99
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-04 00:04:36 -0300

    ✬ Iniciando página de produtos

--- src/produtos/script.js
+++ src/produtos/script.js
@@ -1,24 +1,24 @@
-// import { produtos } from './database/prdutos-db.js'
+import { produtos } from './database/prdutos-db.js'
 
-// const divProduto = document.querySelector('#divProduto');
+const divProduto = document.querySelector('#divProduto');
 
-// function produto(imagem, nome, apresentacao) {
-//     divProduto.innerHTML += `
-//     <div  class="produtoUnit" >    
-//         <img id="img" src="./${imagem}.jpg" alt="">
-//         <h3>${nome}</h3>
-//         <p>${apresentacao}</p>
-//     </div>     
-//     `
-// }
+function produto(imagem, nome, apresentacao) {
+    divProduto.innerHTML += `
+    <div  class="produtoUnit" >    
+        <img id="img" src="./${imagem}.jpg" alt="">
+        <h3>${nome}</h3>
+        <p>${apresentacao}</p>
+    </div>     
+    `
+}
 
-// for (let i = 0; i < produtos.length; i++) {
-//     produto(
-//         produtos[i].imagem,
-//         produtos[i].nome,
-//         produtos[i].apresentacao
-//     )
-// }
+for (let i = 0; i < produtos.length; i++) {
+    produto(
+        produtos[i].imagem,
+        produtos[i].nome,
+        produtos[i].apresentacao
+    )
+}
 
 
 
