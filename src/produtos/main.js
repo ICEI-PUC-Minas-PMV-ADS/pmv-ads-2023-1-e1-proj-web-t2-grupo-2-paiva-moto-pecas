cd76ea1193d920e8858b93bc860736d049f18195
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-12 18:17:10 -0300

    ✬ Add header, footer, wpp.

--- src/produtos/main.js
+++ src/produtos/main.js
@@ -4,8 +4,12 @@ import { resetFilter } from './components/filter-components/reset-filter.js'
 // import { buttonShowMore } from './components/button-show-more.js'
 import { listFilterInputsOptions } from './components/filter.js'
 import { listFilterSearchInput } from './components/search.js'
+import { menu } from '../components/menu.js'
+import { rodape } from '../components/rodape.js'
+import { botaoWpp } from '../components/botao-wpp.js'
 
 export function productsMain() {
+    menu()
     initialSection()
     listFilterInputsOptions()
     const optionsWithHashtag = OptionsWithoutSpaces.map(option => `#${option}`)
@@ -15,7 +19,8 @@ export function productsMain() {
     listFilterSearchInput()
     resetFilter(optionsWithHashtag)
     // buttonShowMore()
+    botaoWpp()
+    rodape()
 }
 
 productsMain()
-
