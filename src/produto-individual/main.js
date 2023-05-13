374291c6b21da77d2e72598231916dd68e1a2972
Ingrid da Rosa Santana de Barros <ingrid.barros.1439833@sga.pucminas.br>
2023-05-12 21:09:13 -0300

    Iniciando página de produto específico

---
+++ src/produto-individual/main.js
@@ -0,0 +1,14 @@
+import {listCards} from "../components/list-cards.js"
+import {products} from "../database/produtos/produtos-db.js"
+
+
+const imagemProduto = document.querySelector("#imagem-produto")
+const listagemProdutos = document.querySelector("#listagem-produtos")
+const randomProducts = products((product)=>{
+
+})
+
+
+
+
+listCards(picture, name, presentation, targetTag, href)
