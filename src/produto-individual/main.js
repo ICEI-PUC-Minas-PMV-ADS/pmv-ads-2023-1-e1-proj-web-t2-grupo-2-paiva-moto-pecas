cbd41ba2dcd0e52bc73d7f2ccbf028ec4ba5aa5a
Ingrid da Rosa Santana de Barros <ingrid.barros.1439833@sga.pucminas.br>
2023-05-12 22:08:18 -0300

    Atualização Produtos Individuais

--- src/produto-individual/main.js
+++ src/produto-individual/main.js
@@ -1,14 +1,21 @@
 import {listCards} from "../components/list-cards.js"
 import {products} from "../database/produtos/produtos-db.js"
+import { menu } from '../components/menu.js'
+import { rodape } from '../components/rodape.js'
+import { botaoWpp } from '../components/botao-wpp.js'
 
+menu()
+rodape()
+botaoWpp()
 
 const imagemProduto = document.querySelector("#imagem-produto")
 const listagemProdutos = document.querySelector("#listagem-produtos")
-const randomProducts = products((product)=>{
-
+products.length = 4
+products.sort(()=> Math.random()-0.5)
+products.map((product)=>{
+listCards(product.picture, product.name, product.presentation, listagemProdutos)
 })
 
 
 
 
-listCards(picture, name, presentation, targetTag, href)
