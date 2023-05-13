71956aa37ab9d5f24122bc9b8574dc3ab4102922
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-12 23:59:33 -0300

    ✬Aside Filter responsivo

--- src/produtos/main.js
+++ src/produtos/main.js
@@ -9,27 +9,8 @@ import { rodape } from '../components/rodape.js'
 import { botaoWpp } from '../components/botao-wpp.js'
 
 
-function filterSection() {
-    const filtersSection = document.querySelector('#filtersSection')
-    filtersSection.innerHTML = `
-        
-    <div id="divFilterTitle">
-            <svg width="31" height="21" viewBox="0 0 31 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                d="M5.21753 11.6717V8.39046H25.2175V11.6717H5.21753ZM0.217529 0.0310898H30.2175V3.39046H0.217529V0.0310898ZM11.8582 20.0311V16.6717H18.5769V20.0311H11.8582Z"
-                fill="black" />
-            </svg>
-            <h1>Filtros</h1>
-            </div>
-    
-            <div id="inputsFilterOptions">
-    </div>    
-    `
-}
-
 export function productsMain() {
     menu()
-    filterSection()
     initialSection()
     listFilterInputsOptions()
     const optionsWithHashtag = OptionsWithoutSpaces.map(option => `#${option}`)
@@ -46,33 +27,70 @@ export function productsMain() {
 productsMain()
 
 const filtersSection = document.querySelector('#filtersSection')
-const divFilterTitle = document.querySelector('#divFilterTitle')
+const aside = document.querySelector('aside')
+const containerFilter = document.querySelector('#container-filter')
+const clickIconFilter = document.querySelector('#click-icon-filter')
+const clickCloseFilter = document.querySelector('#click-close-filter')
 
 addEventListener('resize', () => {
     if (window.innerWidth <= 558) {
-        divFilterTitle.style.display = 'none'
+        containerFilter.style.display = 'none'
+        clickIconFilter.style.display = 'flex'
         filtersSection.style.cssText = `             
         position: sticky;
-        top: 214px;
+        top: 213px;
         left: 60px;
         background-color: var(--cinza-claro);  
         `
-        filtersSection.innerHTML = `
-        <svg id="click-icon-filter" width="31" height="21" viewBox="0 0 31 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.21753 11.6717V8.39046H25.2175V11.6717H5.21753ZM0.217529 
-            0.0310898H30.2175V3.39046H0.217529V0.0310898ZM11.8582 20.0311V16.6717H18.5769V20.0311H11.8582Z"
-            fill="black" />
-        </svg>    
-        `
-    } else {
-        filterSection()
     }
 
-    document.querySelector('#click-icon-filter').addEventListener('click', () => {        
-        filtersSection.style.display = 'flex'
-        filtersSection.style.position = 'absolute'
-        filtersSection.style.left = '50%'
-    })
+    if (window.innerWidth > 558) {
+        containerFilter.style.display = 'grid'
+        clickIconFilter.style.display = 'none'
+        clickCloseFilter.style.display = 'none'
 
+        aside.style.cssText = `
+    padding: 40px 0px;       
+    height: fit-content;       
+    margin: 32px 0px;       
+
+    top: 32px;
+    position: sticky;  
+    z-index: 1;
+
+    border-radius: 12px;
+
+    background-color: var(--branco);     
+    `
+    }
 })
 
+document.querySelector('#click-icon-filter').addEventListener('click', () => {
+    filtersSection.style.cssText = `             
+    position: absolute;
+    top: 257px;     
+    `
+    clickIconFilter.style.display = 'none'
+    containerFilter.style.display = 'grid'
+
+    clickCloseFilter.style.cssText = `
+        display: grid;
+        position: absolute;
+        top: 12px;
+        right: 0px;        
+
+    `
+})
+
+clickCloseFilter.addEventListener('click', () => {
+    containerFilter.style.display = 'none'
+    clickIconFilter.style.display = 'grid'
+    clickCloseFilter.style.display = 'none'
+
+    filtersSection.style.cssText = `             
+    position: sticky;
+    top: 213px;
+    left: 60px;
+    background-color: var(--cinza-claro);      
+    `
+})
