b0ee4fbe32c7c2eeb18900392c30999155955f0f
Alessandra Santos <alessandra.mota@sga.pucminas.br>
2023-05-14 20:01:37 -0300

    Atualização Página Contato

--- src/contato/main.js
+++ src/contato/main.js
@@ -1,12 +1,7 @@
-import { menu } from '../components/menu.js'
-import { rodape } from '../components/rodape.js'
-import { botaoWpp } from '../components/botao-wpp.js'
+import { menu } from '../components/menu.js';
+import { rodape } from '../components/rodape.js';
+import { botaoWpp } from '../components/botao-wpp.js';
 
-
-    menu()
-   
-    botaoWpp()
-    rodape()
-
-
-
+menu();
+botaoWpp();
+rodape();
