972b341081f270c83428ae4df1c83e164110674a
Karina Barbosa <kgbarbosa@sga.pucminas.br>
2023-05-14 17:45:24 -0300

    Atualização js sobre

--- src/sobre/main.js
+++ src/sobre/main.js
@@ -1,5 +1,3 @@
-import {listCards} from "../components/list-cards.js"
-import {products} from "../database/produtos/produtos-db.js"
 import { menu } from '../components/menu.js'
 import { rodape } from '../components/rodape.js'
 import { botaoWpp } from '../components/botao-wpp.js'
@@ -7,14 +5,6 @@ import { botaoWpp } from '../components/botao-wpp.js'
 menu()
 rodape()
 botaoWpp()
-
-const imagemProduto = document.querySelector("#imagem-produto")
-const listagemProdutos = document.querySelector("#listagem-produtos")
-products.length = 4
-products.sort(()=> Math.random()-0.5)
-products.map((product)=>{
-listCards(product.picture, product.name, product.presentation, listagemProdutos)
-})
 
 
 
