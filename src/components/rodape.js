0178532df0bad6d6182beff97557781491e6d0b1
Paloma Rizzon <paloma.rizzon@sga.pucminas.br>
2023-05-16 20:24:41 -0300

    Atualização componente rodapé

--- src/components/rodape.js
+++ src/components/rodape.js
@@ -4,18 +4,18 @@ export function rodape() {
   footer.innerHTML = `
   <div class="container">
         <div>
-          <h3>Endereço</h3>
+          <h3 class="titulo-pequeno">Endereço</h3>
           <p>
             Rua São Sebastião, 1234 Bela Vista - Arapiraca - MG CEP: 12345-678
           </p>
         </div>
         <div>
-          <h3>Contatos</h3>
+          <h3 class="titulo-pequeno">Contatos</h3>
           <p>Telefone: (31)99999-9999</p>
-          <p>Email: contato@paivamotopecas.com</p>
+          <a href="mailto:contato@paivamotopecas.com">Email: contato@paivamotopecas.com</a>
         </div>
         <div>
-          <h3>Horário de funcionamento</h3>
+          <h3 class="titulo-pequeno">Horário de funcionamento</h3>
           <p>Seg a sexta - 08:00 ás 18:00</p>
           <p>Sábado - 09:00 ás 12:00</p>
         </div>
