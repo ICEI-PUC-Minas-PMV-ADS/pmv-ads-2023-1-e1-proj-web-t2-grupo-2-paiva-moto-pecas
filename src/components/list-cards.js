72bc1f2799a4e31060198ddadc6296ec4dc6d807
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-05-18 17:12:15 -0300

    ✬ Aba de serviços.

--- src/components/list-cards.js
+++ src/components/list-cards.js
@@ -1,8 +1,8 @@
-export function listCards(picture, name, presentation, targetTag, database, href) {
+export function listCards(picture, name, presentation, targetTag, directory, href) {
     targetTag.innerHTML += `
     <a href="${href}" class="card" >
         <span>${picture}</span>    
-        <img class="" src="../database/${database}/assets/${picture}.png" alt="">
+        <img class="" src="../database/${directory}/assets/${picture}.png" alt="">
         <h3>${name}</h3>
         <p>${presentation}</p>
         <button class="btn btn-amarelo" type="submit">Adicionar a lista</button>
