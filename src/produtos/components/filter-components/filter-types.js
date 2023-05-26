a2a6a92c09abab656be645692114477a7e115f33
Paloma Rizzon <paloma.rizzon@sga.pucminas.br>
2023-05-26 20:14:30 -0300

    Atualização página produtos

--- src/produtos/components/filter-components/filter-types.js
+++ src/produtos/components/filter-components/filter-types.js
@@ -1,7 +1,9 @@
 export function filterTypes(numOfProductsToJump, FilterType) {
-    const cardsNumber = inputsFilterOptions.querySelectorAll("div");
-    const filteringProductsToRemoveDuplicates = numOfProductsToJump.filter((value, index, arr) => arr.indexOf(value) === index);
-    if (cardsNumber.length === filteringProductsToRemoveDuplicates.length ) {
-        inputsFilterOptions.innerHTML += `<h3 class="h3ByJS">${FilterType}</h3>`
-    }
+  const cardsNumber = inputsFilterOptions.querySelectorAll("div");
+  const filteringProductsToRemoveDuplicates = numOfProductsToJump.filter(
+    (value, index, arr) => arr.indexOf(value) === index
+  );
+  if (cardsNumber.length === filteringProductsToRemoveDuplicates.length) {
+    inputsFilterOptions.innerHTML += `<h2 class="titulo-pequeno h3ByJS">${FilterType}</h2>`;
+  }
 }
--- src/produtos/components/filter-components/responsive-filter.js
+++ src/produtos/components/filter-components/responsive-filter.js
@@ -1,69 +1,63 @@
 export function responsiveFilter() {
-    const filtersSection = document.querySelector('#filtersSection')
-    const aside = document.querySelector('aside')
-    const containerFilter = document.querySelector('#container-filter')
-    const clickIconFilter = document.querySelector('#click-icon-filter')
-    const clickCloseFilter = document.querySelector('#click-close-filter')
+  const filtersSection = document.querySelector("#filtersSection");
+  const aside = document.querySelector("aside");
+  const containerFilter = document.querySelector("#container-filter");
+  const clickIconFilter = document.querySelector("#click-icon-filter");
+  const clickCloseFilter = document.querySelector("#click-close-filter");
 
-    addEventListener('resize', () => {
-        if (window.innerWidth <= 558) {
-            containerFilter.style.display = 'none'
-            clickIconFilter.style.display = 'flex'
-            filtersSection.style.cssText = `             
-        position: sticky;
-        top: 213px;
-        left: 60px;
-        background-color: var(--cinza-claro);  
-        `
-        }
+  addEventListener("resize", () => {
+    if (window.innerWidth <= 558) {
+      containerFilter.style.display = "none";
+      clickIconFilter.style.display = "flex";
+      // filtersSection.style.cssText = `
+      //   background-color: var(--cinza-claro);
+      //   `;
+    }
 
-        if (window.innerWidth > 558) {
-            containerFilter.style.display = 'grid'
-            clickIconFilter.style.display = 'none'
-            clickCloseFilter.style.display = 'none'
+    if (window.innerWidth > 558) {
+      containerFilter.style.display = "grid";
+      clickIconFilter.style.display = "none";
+      clickCloseFilter.style.display = "none";
 
-            aside.style.cssText = `
-    padding: 40px 0px;       
-    height: fit-content;       
-    margin: 32px 0px;       
+      //    aside.style.cssText = `
+      //  padding: 40px 0px;
+      //  height: fit-content;
+      //  margin: 32px 0px;
+      //  top: 32px;
+      //  position: sticky;
+      //  z-index: 1;
+      //  border-radius: 12px;
+      //  background-color: var(--branco);
+      //  `;
+    }
+  });
 
-    top: 32px;
-    position: sticky;  
-    z-index: 1;
-
-    border-radius: 12px;
-
-    background-color: var(--branco);     
-    `
-        }
-    })
-
-    document.querySelector('#click-icon-filter').addEventListener('click', () => {
-        filtersSection.style.cssText = `             
+  document.querySelector("#click-icon-filter").addEventListener("click", () => {
+    filtersSection.style.cssText = `             
     position: absolute;
     top: 257px;     
-    `
-        clickIconFilter.style.display = 'none'
-        containerFilter.style.display = 'grid'
+    `;
+    clickIconFilter.style.display = "none";
+    containerFilter.style.display = "grid";
 
-        clickCloseFilter.style.cssText = `
+    clickCloseFilter.style.cssText = `
         display: grid;
         position: absolute;
         top: 12px;
         right: 12px;        
-    `
-    })
+    `;
+  });
 
-    clickCloseFilter.addEventListener('click', () => {
-        containerFilter.style.display = 'none'
-        clickIconFilter.style.display = 'grid'
-        clickCloseFilter.style.display = 'none'
+  clickCloseFilter.addEventListener("click", () => {
+    containerFilter.style.display = "none";
+    clickIconFilter.style.display = "grid";
+    clickCloseFilter.style.display = "none";
 
-        filtersSection.style.cssText = `             
+    filtersSection.style.cssText = `             
     position: sticky;
     top: 213px;
     left: 60px;
     background-color: var(--cinza-claro);      
-    `
-    })
+    `;
+  });
 }
