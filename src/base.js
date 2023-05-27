dafcf452527cafb8692af5cc1db15646fd5e8895
Paloma Rizzon <paloma.rizzon@sga.pucminas.br>
2023-05-27 20:40:16 -0300

    Correção linkagem para produto individual

--- src/base.js
+++ src/base.js
@@ -1,24 +1,45 @@
+import { products } from "./database/produtos/produtos-db.js";
+import { servicos } from "./database/serviços/servicos-db.js";
+
 export function preencherElem() {
   const contElements = document.getElementById("cont-principal");
   const card = document.querySelector(".go-page");
 
-  card.addEventListener("click", () => {
-    const pegarId = card.querySelector("span");
-    const elemUnico = products.find((product) => {
-      product.picture === pegarId;
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const produtos = urlParams.get("produtos");
+  const servicosurl = urlParams.get("serviços");
+  let elemUnico;
+
+  if (!servicosurl) {
+    elemUnico = products.find((product) => {
+      return product.picture === produtos;
     });
-    contElements.innerHTML = ` 
+  } else if (!produtos) {
+    elemUnico = servicos.find((servico) => {
+      return servico.picture === servicosurl;
+    });
+  }
+
+  contElements.innerHTML = ` 
     <div id="container-produto">
-    <img id="imagem-produto" src="../database/produtos/assets/${elemUnico.picture}.png" alt="">
+    <img id="imagem-produto" src="../database/${
+      servicosurl ? "serviços" : "produtos"
+    }/assets/${elemUnico.picture}.png" alt="">
     <div id="container-info">
         <h1 class="titulo-medio">${elemUnico.name}</h1>
         <p>${elemUnico.description}
         </p>
-        <h2 class="titulo-pequeno"><strong>Marca: </strong>${elemUnico.brand}</h2>
-        <h2 class="titulo-pequeno"><strong>Categoria: </strong>${elemUnico.category}</h2>
+        <h2 class="titulo-pequeno">${
+          elemUnico.brand
+            ? `<strong>Marca: </strong>${elemUnico.brand}</h2>`
+            : ""
+        }
+        <h2 class="titulo-pequeno"><strong>Categoria: </strong>${
+          elemUnico.category
+        }</h2>
     </div>
 </div>
 <button class="btn btn-amarelo add-button">Adicionar a lista</button>
     `;
-  });
 }
--- src/components/list-cards.js
+++ src/components/list-cards.js
@@ -7,9 +7,10 @@ export function listCards(
   href
 ) {
   targetTag.innerHTML += `
-    <a href="../produto-individual/index.html" class="card go-page">
-        <span>${picture}</span>    
-        <img src="../database/${directory}/assets/${picture}.png" alt="">
+    <a href="../produto-individual/index.html?${directory}=${picture}" class="card go-page">
+        <img style="${
+          directory == "serviços" ? "object-fit:cover" : ""
+        }" src="../database/${directory}/assets/${picture}.png" alt="">
         <h3>${name}</h3>
         <p>${presentation}</p>
         <button class="btn btn-amarelo add-button" type="submit">Adicionar a lista</button>
