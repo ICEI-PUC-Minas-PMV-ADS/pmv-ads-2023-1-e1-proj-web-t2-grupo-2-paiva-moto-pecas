3e38ac23db313253bb688693d7281e5597db1515
Paloma Rizzon <paloma.rizzon@sga.pucminas.br>
2023-06-08 20:03:52 -0300

    Atualização componente list cards + css

--- src/components/list-cards.js
+++ src/components/list-cards.js
@@ -1,18 +1,17 @@
-export function listCards(
-  picture,
-  name,
-  presentation,
-  targetTag,
-  directory,  
-) {
+export function listCards(picture, name, presentation, targetTag, directory) {
   targetTag.innerHTML += `
-    <a href="../produto-individual/index.html?${directory}=${picture}" class="card go-page">
+  <div class="card">
+    <a href="../produto-individual/index.html?${directory}=${picture}">
         <img style="${
           directory == "serviços" ? "object-fit:cover" : ""
         }" src="../database/${directory}/assets/${picture}.png" alt="">
         <h3>${name}</h3>
         <p>${presentation}</p>
-        <button value="${[picture, directory]}" class="btn btn-amarelo add-button" type="submit">Adicionar a lista</button>
-    </a>     
+    </a>
+    <button value="${[
+      picture,
+      directory,
+    ]}" class="btn btn-amarelo add-button" type="submit">Adicionar a lista</button>
+    </div>
     `;
 }
