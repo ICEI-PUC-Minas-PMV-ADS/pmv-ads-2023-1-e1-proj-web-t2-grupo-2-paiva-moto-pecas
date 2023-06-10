bc528172a330dc002128842cc741b7a01c6a10c8
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-06-10 18:39:27 -0300

    ✬ Estabelecendo o setor inicial

--- src/itens-da-loja/main.js
+++ src/itens-da-loja/main.js
@@ -13,30 +13,56 @@ import {
 } from "./components/filter-components/options.js";
 import { pageshow } from "./components/pageshow.js";
 import { filter } from "./components/filter.js";
+import { adicionaAlista } from "../base.js";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const url = new URL(window.location.href);
+const initialSector = urlParams.get("sector");
 
 menu();
 botaoWpp();
 rodape();
-(function main(database, options, assets) {
 
+let database, options, assets;
+
+if (initialSector !== 'serviços') {
+  database = products;
+  options = OptionsWithoutSpacesProducts;
+  assets = "produtos";
+} else {
+  database = servicos;
+  options = OptionsWithoutSpacesServices;
+  assets = "serviços";
+}
+
+(function main(database, options, assets) {
   initialSection(database, assets);
   listFilterInputsOptions(database);
   filter(database, assets)
   showMoreFullDB(database);
   responsiveFilter();
-
+  adicionaAlista();
   pageshow(database, assets);
-  const title = document.querySelector("title");
+
+  const buttonShowMore = document.querySelector("#button-show-more");
   const buttonsChangePage = document.querySelector("#buttonsChangePage");
   const productsButton = buttonsChangePage.querySelector("button:nth-child(1)");
   const servicesButton = buttonsChangePage.querySelector("button:nth-child(2)");
-  const buttonShowMore = document.querySelector("#button-show-more");
+
+  if (assets === 'produtos') {    
+    productsButton.style.backgroundColor = "var(--amarelo-secundario)";
+  } else {   
+    servicesButton.style.backgroundColor = "var(--amarelo-secundario)";
+  }
+  buttonShowMore.innerText = `Ver mais ${(database[0].sector).toLowerCase()}s`;
+
+  const dynamicServicesButton = database === products ? servicesButton : productsButton;
+  const dynamicProductsButton = database === servicos ? productsButton : servicesButton;
 
   function setSector(button, sectorDB, optionsSector) {
     if (database !== sectorDB) {
-      button.addEventListener("click", () => {
-        title.innerText = `Paiva Moto Peças | ${sectorDB[0].sector}s`;
-        buttonShowMore.innerText = `Ver mais ${sectorDB[0].sector}s`;
+      button.addEventListener("click", () => {        
         const dynamicServicesButton = button === productsButton ? servicesButton : productsButton;
         const dynamicProductsButton = button === productsButton ? productsButton : servicesButton;
 
@@ -49,9 +75,6 @@ rodape();
   setSector(servicesButton, servicos, OptionsWithoutSpacesServices)
   setSector(productsButton, products, OptionsWithoutSpacesProducts)
 
-})(products, OptionsWithoutSpacesProducts, "produtos");
-
-
-
+})(database, options, assets);
 
 
